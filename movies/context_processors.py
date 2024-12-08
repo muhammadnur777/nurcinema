15360e982ecac9bdbc1aed2f4bca5dbf0bc998e2
unknown <muhammadnuryusufjonov31@gmail.com>
2024-12-08 11:16:31 +0500

    context udate

--- movies/context_processors.py
+++ movies/context_processors.py
@@ -2,7 +2,6 @@ from .models import Movie
 
 
 def movie_detail_slug(request):
-    movie_detail_slug = Movie.objects.first().slug
-
-
-    return {'movie_detail_slug': movie_detail_slug}
+    if Movie.objects.first():
+        movie_detail_slug = Movie.objects.first().slug
+        return {'movie_detail_slug': movie_detail_slug}
